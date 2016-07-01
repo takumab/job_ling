6a0f291832bce0e1841bd3ef4d57554d74c5e304
takumab <takumab@gmail.com>
2016-07-01 12:24:10 +0100

    Add more styling to forms and experimented with different colors for job cards

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,5 @@ class Job < ActiveRecord::Base
 	validates :position, presence: true
 		validates :description, presence: true
 		validates :company, presence: true
+		validates :city, presence: true
 end
