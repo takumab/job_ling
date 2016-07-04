2a3814d70f9159022737beb89cc87126b9a3b7ed
takumab <takumab@gmail.com>
2016-07-04 15:05:43 +0100

    Add job categories to navbar and jobs controller and changed the root route to landing page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,12 @@ class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
 
   def index
-  	@jobs = Job.all.order("created_at DESC")
+    if params[:category].blank?
+  	 @jobs = Job.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def new
@@ -45,6 +50,6 @@ class JobsController < ApplicationController
   end
 
   def job_params
-  	params.require(:job).permit(:position, :description, :company, :city)
+  	params.require(:job).permit(:position, :description, :company, :city, :category_id)
   end
 end
