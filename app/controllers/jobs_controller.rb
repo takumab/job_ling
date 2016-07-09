4dab2350260595797077012d28d8530420f491de
takumab <takumab@gmail.com>
2016-07-09 12:13:00 +0100

    Add paginate to fix error for categories, interships, fulltime, etc

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,7 +6,7 @@ class JobsController < ApplicationController
   	 @jobs = Job.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
     else
       @category_id = Category.find_by(name: params[:category]).id
-      @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+      @jobs = Job.where(category_id: @category_id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
     end
   end
 
