2c239995e5ccd2bb92e7b229dfbbbff38f3e9145
takumab <takumab@gmail.com>
2016-07-15 11:20:37 +0100

    Removed location again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703162657) do
+ActiveRecord::Schema.define(version: 20160715101136) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
